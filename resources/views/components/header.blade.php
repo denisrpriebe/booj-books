513deb528e6047e6450f705a4f63eeb586833c82
Denis Priebe <denis.priebe@irishtitan.com>
2019-11-13 20:23:41 -0600

    Update header layout.

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row justify-content-center pt-4">
+        <div class="col-md-8">
+            <h1 class="text-center">Booj Books</h1>
+            <hr>
+        </div>
+    </div>
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 <body>
     <div id="app">
 
-        @include('components.navbar')
+        @include('components.header')
 
         <main class="py-4">
             @yield('content')
